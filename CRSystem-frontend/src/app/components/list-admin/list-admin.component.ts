67d2c0f35b8ee0622cc9bf04bd90777f70c9f2fd
Bharat Kumar P <bharatkumarp28@gmail.com>
2022-02-17 08:18:32 +0530

    SpringBoot and Angular Basic Curd

---
+++ CRSystem-frontend/src/app/components/list-admin/list-admin.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Admin } from 'src/app/models/admin';
+import { AdminService } from 'src/app/services/admin.service';
+
+@Component({
+  selector: 'app-list-admin',
+  templateUrl: './list-admin.component.html',
+  styleUrls: ['./list-admin.component.css']
+})
+export class ListAdminComponent implements OnInit {
+
+  admin: Admin[] = [];
+
+  constructor(private _adminService: AdminService) { }
+
+  ngOnInit(): void {
+    this._adminService.getAdmin().subscribe(
+      data => this.admin = data
+    )
+  }
+
+}
